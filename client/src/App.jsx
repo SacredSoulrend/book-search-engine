a3e52af655479b80649f1d6b0fcaeb4910f4f022
Son Giang <sgiangt@gmail.com>
2023-12-01 01:36:17 -0700

    added backend and frontend specifications - it took a couple of days to figure this out

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,42 @@
 import './App.css';
+import React from 'react';
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import { Outlet } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
 
+// GraphQL endpoint
+const graphqlEndpoint = '/graphql';
+
+// Create an HTTP link to the GraphQL endpoint
+const httpLink = createHttpLink({
+  uri: graphqlEndpoint,
+});
+
+// Authorization middleware to attach JWT token to requests
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+// Create an Apollo Client instance
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
-    <>
+    <ApolloProvider client={client}>
       <Navbar />
       <Outlet />
-    </>
+    </ApolloProvider>
   );
 }
 
---
+++ server/schemas/resolvers.js
@@ -0,0 +1,82 @@
+const { AuthenticationError } = require("apollo-server-express");
+const { User } = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+  Query: {
+    // Resolver to get the current user's data
+    me: async (_parent, _args, context) => {
+      // Check if the user is authenticated
+      if (context.user) {
+        // Retrieve user data without password, including saved books
+        const userData = await User.findById(context.user._id).select('-__v -password').populate('savedBooks');
+        return userData;
+      }
+      throw new AuthenticationError('No user found');
+    },
+  },
+  Mutation: {
+    // Resolver for user login
+    login: async (_parent, { email, password }) => {
+      // Find user by email
+      const user = await User.findOne({ email });
+
+      // Check if the user exists and the password is correct
+      if (!user || !(await user.isCorrectPassword(password))) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      // Generate a token for the authenticated user
+      const token = signToken(user);
+      return { token, user };
+    },
+
+    // Resolver for saving a book to the user's account
+    saveBook: async (_parent, { bookData }, context) => {
+      // Check if the user is authenticated
+      if (context.user) {
+        // Find the user by ID and update the savedBooks array
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $addToSet: { savedBooks: bookData } }, // Use $addToSet to add the book if it doesn't exist
+          { new: true } // Return the updated user object
+        );
+        return updatedUser;
+      }
+      throw new AuthenticationError('Not logged in');
+    },
+
+    // Resolver for adding a new user
+    addUser: async (_parent, args) => {
+      // Create a new user
+      const user = await User.create(args);
+
+      // Check if the user was successfully created
+      if (!user) {
+        // Throw an error if the user creation failed
+        throw new AuthenticationError('Failed to create user');
+      }
+
+      // Generate a token for the newly created user
+      const token = signToken(user);
+      return { token, user };
+    },
+
+    // Resolver for removing a saved book from the user's account
+    removeBook: async (_parent, { bookId }, context) => {
+      // Check if the user is authenticated
+      if (context.user) {
+        // Find the user by ID and pull the specified book from savedBooks
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { savedBooks: { bookId } } },
+          { new: true } // Return the updated user object
+        );
+        return updatedUser;
+      }
+      throw new AuthenticationError('Not logged in');
+    },
+  },
+};
+
+module.exports = resolvers;
